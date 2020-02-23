6956d102f04626ca545e67cf0bf3bf5e3592b726
Matt Mittelsted <matthewthewebdev@gmail.com>
2020-02-23 11:52:07 -0700

    change ng-container to div
    maybe the issue lies with the use of ng-container

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -10,7 +10,7 @@ import { FlashMessagesService } from 'angular2-flash-messages';
 })
 export class NavbarComponent implements OnInit {
   isCollapsed: boolean = true;
-  isLoggedIn: boolean;
+  isLoggedIn: boolean = false;
   currentUser: string;
   // showRegister: boolean;
 
